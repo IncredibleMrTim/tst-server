80e3c55d8125e13b531c6eb4d29fd50174d13f86
Smart, Tim (The Bridge) <tim.smart@timsmarttechnology.co.uk>
2025-03-17 19:57:05 +0000

    trying to run on vercel

--- collections/experience.js
+++ collections/experience.js
@@ -1,28 +1,30 @@
 import { getDatabase, db } from '../db.js';
 
+db();
+
 export async function getExperiences() {
-    await db()
+
     const database = getDatabase();
     const experiences = await database.collection('experience').find({}).toArray();
     return experiences;
 }
 
 export async function addExperience(experience) {
-    await db()
+
     const database = getDatabase();
     const result = await database.collection('experience').insertOne(experience);
     return result;
 }
 
 export async function updateExperience(id, experience) {
-    await db()
+
     const database = getDatabase();
     const result = await database.collection('experience').updateOne({ _id: id }, { $set: experience });
     return result;
 }
 
 export async function deleteExperience(id) {
-    await db()
+
     const database = getDatabase();
     
     const result = await database.collection('experience').deleteOne({ _id: id });
