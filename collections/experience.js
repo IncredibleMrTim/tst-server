33f7fee5bafc6c785fbb3b15afacc0a6de162759
Smart, Tim (The Bridge) <tim.smart@timsmarttechnology.co.uk>
2025-03-17 19:52:43 +0000

    trying to run on vercel

--- collections/experience.js
+++ collections/experience.js
@@ -2,25 +2,28 @@ import { getDatabase, db } from '../db.js';
 
 export async function getExperiences() {
     await db()
-    const database = getDatabase();
-    const experiences = await database.collection('experience').find({}).toArray();
+    const db = getDatabase();
+    const experiences = await db.collection('experience').find({}).toArray();
     return experiences;
 }
 
-// export async function addExperience(experience) {
-//     const db = getDatabase();
-//     const result = await db.collection('experience').insertOne(experience);
-//     return result;
-// }
+export async function addExperience(experience) {
+    await db()
+    const db = getDatabase();
+    const result = await db.collection('experience').insertOne(experience);
+    return result;
+}
 
-// export async function updateExperience(id, experience) {
-//     const db = getDatabase();
-//     const result = await db.collection('experience').updateOne({ _id: id }, { $set: experience });
-//     return result;
-// }
+export async function updateExperience(id, experience) {
+    await db()
+    const db = getDatabase();
+    const result = await db.collection('experience').updateOne({ _id: id }, { $set: experience });
+    return result;
+}
 
-// export async function deleteExperience(id) {
-//     const db = getDatabase();
-//     const result = await db.collection('experience').deleteOne({ _id: id });
-//     return result;
-// }
+export async function deleteExperience(id) {
+    await db()
+    const db = getDatabase();
+    const result = await db.collection('experience').deleteOne({ _id: id });
+    return result;
+}
