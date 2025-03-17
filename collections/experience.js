21ecbd23b30d95ab061340acdc8aac539b5e2f18
Smart, Tim (The Bridge) <tim.smart@timsmarttechnology.co.uk>
2025-03-17 18:38:06 +0000

    init

---
+++ collections/experience.js
@@ -0,0 +1,25 @@
+import { getDatabase } from '../db.js';
+
+export async function getExperiences() {
+    const db = getDatabase();
+    const experiences = await db.collection('experience').find({}).toArray();
+    return experiences;
+}
+
+export async function addExperience(experience) {
+    const db = getDatabase();
+    const result = await db.collection('experience').insertOne(experience);
+    return result;
+}
+
+export async function updateExperience(id, experience) {
+    const db = getDatabase();
+    const result = await db.collection('experience').updateOne({ _id: id }, { $set: experience });
+    return result;
+}
+
+export async function deleteExperience(id) {
+    const db = getDatabase();
+    const result = await db.collection('experience').deleteOne({ _id: id });
+    return result;
+}
